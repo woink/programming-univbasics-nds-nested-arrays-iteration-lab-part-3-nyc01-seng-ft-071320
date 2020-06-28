ae8bb6b0ba0ddc16ee28f24ee1dc50fc883ad11e
Ward Price <wardprice@gmail.com>
2020-06-28 16:30:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,5 +6,11 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0 
     while element_index < src[row_index].count do 
-      if src[row_index][element_index]
+      if src[row_index][element_index].class = "String" do
+        phrase += src[row_index][element_index]
+        element_index += 1
+      end
+      row_index += 1 
+    end
+    phrase
 end
